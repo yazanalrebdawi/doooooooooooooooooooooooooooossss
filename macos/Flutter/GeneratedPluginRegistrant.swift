19690c3e7b699ee83279d3024257b7a4b245ccf1
mohamed-zeytoun123 <bdrabw74@gmail.com>
2025-09-30 00:34:59 +0300

    test last download

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,30 +5,22 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
 import file_selector_macos
 import flutter_secure_storage_macos
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
-<<<<<<< HEAD
-=======
 import sqflite_darwin
->>>>>>> zoz
 import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-<<<<<<< HEAD
-=======
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
->>>>>>> zoz
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
